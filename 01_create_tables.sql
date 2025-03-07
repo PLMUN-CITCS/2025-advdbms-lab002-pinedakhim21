1728a33f96a01d043f92a4da35c5fac6c2dd6a3b
pinedakhim21 <pinedakhimberly_bsit@plmun.edu.ph>
2025-03-07 17:16:11 +0800

    Completed assignment: Created tables and inserted data

---
+++ 01_create_tables.sql
@@ -0,0 +1,7 @@
+-- Creating tblStudents table
+CREATE TABLE tblStudents (
+    student_id INT PRIMARY KEY AUTO_INCREMENT,
+    student_name VARCHAR(50),
+    student_email VARCHAR(50),
+    student_phone VARCHAR(50)
+);
---
+++ 02_insert_data.sql
@@ -0,0 +1,6 @@
+-- Creating tblStudents table
+-- Inserting data into tblStudents
+INSERT INTO tblStudents (student_name, student_email, student_phone)
+VALUES
+    ('John Doe', 'johndoe@gmail.com', '123-456-7890'),
+    ('Jane Doe', 'janedoe@gmail.com', '123-456-7890');
